5a07faa007007df749b786815e532d65e23974a0
xArmeriumx <armnapat.a.03@gmail.com>
2025-12-28 18:03:33 +0700

    Update Project-1.9 (Page Lazy)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// ใช้ /Portfolio-Napat/ สำหรับ GitHub Pages, "/" สำหรับ Vercel
+const base = process.env.GITHUB_PAGES ? "/Portfolio-Napat/" : "/";
+
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: base,
 });
-
