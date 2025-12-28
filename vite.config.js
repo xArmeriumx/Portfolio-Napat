7b143a3d5d41c1e79559a5efa58030e436f6f7c1
xArmeriumx <armnapat.a.03@gmail.com>
2025-12-28 20:35:05 +0700

    Update Project-2.1 (404 Page Not Found)

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,12 @@ const base = process.env.GITHUB_PAGES ? "/Portfolio-Napat/" : "/";
 export default defineConfig({
   plugins: [react()],
   base: base,
+  build: {
+    sourcemap: false, // ปิด SourceMap
+    minify: 'esbuild', // ย่อโค้ดให้เล็กที่สุด
+    chunkSizeWarningLimit: 1000,
+  },
+  esbuild: {
+    drop: ['console', 'debugger'], // ลบ console.log และ debugger ทิ้งทั้งหมด
+  },
 });
